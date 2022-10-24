7410a58d409262bd1d7e6647fcace03b298ed08c
Tyler Davies <45601586+qShadxw@users.noreply.github.com>
2022-10-24 13:57:07 +0100

    Reverted back to HTML.

--- settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-$settings = (
-  "indexTitle" => "Natunori",
-  "earthshipTitle" => "House of the Future",
-  "houseTitle" => "House of the Future",
-  "musicTitle" => "Natunori",
-  "priddawomanTitle" => "Natunori",
-  "profilesTitle" => "Natunori",
-)
